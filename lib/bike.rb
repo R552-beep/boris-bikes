373f67d948e617fa9f7c19495f824c15bcc9fa2b
Ruzeka Uddin <ruzekauddin@Ruzekas-MacBook-Air.local>
2022-03-25 14:03:20 +0000

    chap18 Dealing with broken bikes

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,5 +1,11 @@
 class Bike
+    
   def working?
     true
   end    
+
+ def report_broken
+    true
+ end
+
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,5 +4,7 @@ describe Bike do
   bike = Bike.new
     expect(bike.working?).to eq true
   end
-    
+    it 'can report if broken' do
+    expect(subject.report_broken).to be_truthy
+ end
 end
